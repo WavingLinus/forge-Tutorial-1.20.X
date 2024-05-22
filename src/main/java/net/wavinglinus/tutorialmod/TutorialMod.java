f16da49731703a7c9b326f0f3f47154b816c9714
WavingLinus <linus26@protonmail.com>
2024-05-23 00:15:51 +0200

    .

--- src/main/java/net/wavinglinus/tutorialmod/TutorialMod.java
+++ src/main/java/net/wavinglinus/tutorialmod/TutorialMod.java
@@ -17,6 +17,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.wavinglinus.tutorialmod.items.ModCreativeModTabs;
 import net.wavinglinus.tutorialmod.items.ModItems;
 import org.slf4j.Logger;
 
@@ -30,6 +31,8 @@ public class TutorialMod {
 
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
 
@@ -48,6 +51,7 @@ public class TutorialMod {
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
            event.accept(ModItems.SAPPHIRE);
+           event.accept(ModItems.RAW_SAPPHIRE);
        }
     }
 
